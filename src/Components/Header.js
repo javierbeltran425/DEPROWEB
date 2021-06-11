b8328e6b6c3744376d6cb16394d7b42f14014383
javierbeltran425 <00019315@uca.edu.sv>
2021-06-11 16:21:51 -0600

    Correction

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -96,7 +96,7 @@ const Header = () => {
             </div>
             <div className="flex md:flex-row items-center">
                 <Icon icon={faPhoneAlt} className="text-blue-300 m-2"/>
-                <p className="px-2"><a href="tel:+50322524892">+503 2252-4892</a></p>
+                <p className="px-2"><a href="tel:+50322524892">2252-4892</a></p>
             </div>
             <div className="hidden md:flex md:flex-row md:items-center">
                 <Icon icon={faClock} className="text-blue-300"/>
@@ -122,7 +122,7 @@ const Header = () => {
             </div>
             <div className="flex md:flex-row items-center">
                 <Icon icon={faPhoneAlt} className="text-blue-300 m-2"/>
-                <p className="px-2"><a href="tel:+50322524892">+503 2252-4892</a></p>
+                <p className="px-2"><a href="tel:+50322524892">2252-4892</a></p>
             </div>
             <div className="hidden md:flex md:flex-row md:items-center">
                 <Icon icon={faClock} className="text-blue-300"/>
