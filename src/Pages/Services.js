ca74dec8a645c95896c59e7ba2f9d20a9cd2a03c
javierbeltran425 <00019315@uca.edu.sv>
2021-08-30 21:41:48 -0600

    Redesign of services view

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,5 +1,6 @@
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
+import ServicesCard from '../Components/OurServicesCard'
 
 import Whatsapp from '../resources/whatsappLogo.png'
 
@@ -7,11 +8,10 @@ const Services = () => {
     return(
         <>
             <Header />
-            <div className="w-full h-screen bg-gray-300">
+            <div className="w-full h-full bg-gray-300">
                 <h1 className="font-bold text-center p-10 text-3xl">NUESTROS SERVICIOS</h1>
-                <div className="grid grid-cols-4 divide-x-2 divide-gray-400 divide-solid">
-                    <div className="w-full h-full">
-                        <h2 className="font-bold p-2 text-blue-700 text-xl text-center mb-10">INGENIERIA Y PROYECTOS</h2>
+                <div className="flex flex-row flex-wrap justify-center pb-5">
+                    <ServicesCard title="INGENIERIA Y PROYECTOS" content={
                         <ul className="list-disc px-5">
                             <li className="hover:text-blue-600">AUTOMATIZACION DE PROCESOS INDUSTRIALES Y SISTEMAS SCADA.</li>
                             <li className="hover:text-blue-600">MODIFICACION Y RESTAURACION DE PANELES ELECTRICOS.</li>
@@ -22,43 +22,39 @@ const Services = () => {
                             <li className="hover:text-blue-600">PROTECCIONES ATMOSFERICAS (PARARRAYOS)</li>
                             <li className="hover:text-blue-600">CONTROL DE TIEMPOS MUERTOS EN MÁQUINA</li>
                         </ul>
-                    </div>
+                    }/>
 
-                    <div className="w-full h-full">
-                        <h2 className="font-bold p-2 text-blue-700 text-xl text-center mb-10">MANTENIMIENTOS PREVENTIVOS Y CORRECTIVOS</h2>
+                    <ServicesCard title="MANTENIMIENTOS PREVENTIVOS Y CORRECTIVOS" content={
                         <ul className="list-disc px-5">
-                            <li>SUB-ESTACIONES ELECTRICAS.</li>
-                            <li>REDES DE TIERRA.</li>
-                            <li>CHILLERS, MANEJADORAS DE AIRE Y AIRES ACONDICIONADOS.</li>
-                            <li>MOTORES MONOFASICOS Y TRIFASICOS.</li>
-                            <li>UPS MONOFASICOS Y TRIFASICOS.</li>
-                            <li>TABLEROS ELECTRICOS DE CONTROL.</li>
-                        </ul>
-                    </div>
+                        <li className="hover:text-blue-600">SUB-ESTACIONES ELECTRICAS.</li>
+                        <li className="hover:text-blue-600">REDES DE TIERRA.</li>
+                        <li className="hover:text-blue-600">CHILLERS, MANEJADORAS DE AIRE Y AIRES ACONDICIONADOS.</li>
+                        <li className="hover:text-blue-600">MOTORES MONOFASICOS Y TRIFASICOS.</li>
+                        <li className="hover:text-blue-600">UPS MONOFASICOS Y TRIFASICOS.</li>
+                        <li className="hover:text-blue-600">TABLEROS ELECTRICOS DE CONTROL.</li>
+                    </ul>
+                    }/>
 
-                    <div className="w-full h-full">
-                        <h2 className="font-bold p-2 text-blue-700 text-xl text-center mb-10">SOPORTE TECNICO</h2>
+                    <ServicesCard title="SOPORTE TECNICO" content={
                         <ul className="list-disc px-5">
-                            <li>PROGRAMACION DE VARIADORES DE FRECUENCIA.</li>
-                            <li>DIAGNOSTICOS EN PLC, VARIADORES DE FRECUENCIA Y HMI.</li>
-                            <li>REPROGRAMACION DE PLC Y HMI.</li>
-                            <li>INSTALACION DE UPS Y SUPRESORES DE TRANSIENTES.</li>
-                            <li>CABLEADO DE PANELES DE CONTROL.</li>
+                            <li className="hover:text-blue-600">PROGRAMACION DE VARIADORES DE FRECUENCIA.</li>
+                            <li className="hover:text-blue-600">DIAGNOSTICOS EN PLC, VARIADORES DE FRECUENCIA Y HMI.</li>
+                            <li className="hover:text-blue-600">REPROGRAMACION DE PLC Y HMI.</li>
+                            <li className="hover:text-blue-600">INSTALACION DE UPS Y SUPRESORES DE TRANSIENTES.</li>
+                            <li className="hover:text-blue-600">CABLEADO DE PANELES DE CONTROL.</li>
                         </ul>
-                    </div>
+                    }/>
 
-                    <div className="w-full h-full">
-                        <h2 className="font-bold p-2 text-blue-700 text-xl text-center mb-10">CALIDAD DE ENERGIA</h2>
+                    <ServicesCard title="CALIDAD DE ENERGIA" content={
                         <ul className="list-disc px-5">
-                            <li>CORRECCION DE FACTOR DE POTENCIA.</li>
-                            <li>ANALISIS DE CALIDAD DE ENERGIA.</li>
-                            <li>SISTEMAS DE ALIMENTACION ININTERRUMPIDA(UPS).</li>
-                            <li>SUPRESORES DE TRANSIENTES.</li>
-                            <li>AHORRO ENERGETICO.</li>
-                            <li>SISTEMA SCADA PARA MONITOREO DE ENERGIA.</li>
+                            <li className="hover:text-blue-600">CORRECCION DE FACTOR DE POTENCIA.</li>
+                            <li className="hover:text-blue-600">ANALISIS DE CALIDAD DE ENERGIA.</li>
+                            <li className="hover:text-blue-600">SISTEMAS DE ALIMENTACION ININTERRUMPIDA(UPS).</li>
+                            <li className="hover:text-blue-600">SUPRESORES DE TRANSIENTES.</li>
+                            <li className="hover:text-blue-600">AHORRO ENERGETICO.</li>
+                            <li className="hover:text-blue-600">SISTEMA SCADA PARA MONITOREO DE ENERGIA.</li>
                         </ul>
-                    </div>
-
+                    }/>
                 </div>
                 <a href="https://api.whatsapp.com/send?phone=50378532004" className="fixed z-10 w-10 h-10 md:w-16 md:h-16 m-10 right-0 bottom-0"><span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-900 opacity-75"></span><img src={Whatsapp}/></a>
             </div>
