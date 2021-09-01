05e40d729131ab0789fab86783ac8394e634e4a7
javierbeltran425 <00019315@uca.edu.sv>
2021-09-01 13:52:53 -0600

    Pay alerts

--- src/Components/SystemLocked.js
+++ src/Components/SystemLocked.js
@@ -2,18 +2,17 @@ const SystemLocked = () => {
     return(
         <div className={"fixed flex justify-center items-center w-screen h-screen z-10"}>
             <div className="w-full h-full bg-gray-900 opacity-80"/>
-            <div className="absolute flex flex-col w-1/2 h-1/2 bg-white rounded-lg">
+            <div className="absolute flex flex-col w-1/2 h-1/2 bg-red-200 rounded-lg">
                 <div className="flex flex-col justify-center w-full h-full p-4">
                     <h1 className="text-center font-extrabold">¡NOTIFICACIÓN DE SUSPENCIÓN POR ATRASO DE PAGO!</h1>
                     <p className="text-center font-bold mt-10">
                     Estimado usuario, se le notifica que por falta de pago de desarrollo de sistema se 
-                    le ha restringido el uso del panel de administracion. 
+                    ha restringido el uso del panel de administracion. 
                     Se recomienda realizar  el pago a la brevedad posible para restablecer el servicio.
                     <br/><br/>
                     En cuanto el pago sea efectivo se recuperara el uso normal del sistema.
                     </p>
                 </div>
-                
             </div>
         </div>
     )
